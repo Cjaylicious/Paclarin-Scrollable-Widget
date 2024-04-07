8a0eff9ab5fb27ccd0b851f001025a004748e3f5
unknown <samboy09082018@gmail.com>
2024-04-07 11:46:48 +0800

    first commit

---
+++ starter/lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      scrollBehavior: CustomSCrollBehavior(),
+      home: const Home(),
+    );
+  }
+}
+
+class CustomSCrollBehavior extends MaterialScrollBehavior{
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+  };
+}
